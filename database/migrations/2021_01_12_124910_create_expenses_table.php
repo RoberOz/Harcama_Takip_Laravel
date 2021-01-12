e63bfc3d3bec8e1bc7f2afaf77aa311ac9e15e4d
RoberOz <roberrudi006@gmail.com>
2021-01-12 16:21:02 +0300

    Migartion Fix

--- database/migrations/2021_01_12_124910_create_expenses_table.php
+++ database/migrations/2021_01_12_124910_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Expenses extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Expenses extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('category_id');
+            $table->bigInteger('category_id');
             $table->integer('amount');
             $table->date('date');
             $table->string('location', 70);
