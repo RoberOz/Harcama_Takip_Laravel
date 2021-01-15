9c5b729d095e4be2d5b4785025d95f6871feef39
RoberOz <roberrudi006@gmail.com>
2021-01-15 12:17:34 +0300

    laravel 5.5

--- config/session.php
+++ config/session.php
@@ -161,7 +161,6 @@ return [
     */
 
     'secure' => env('SESSION_SECURE_COOKIE', false),
-
     /*
     |--------------------------------------------------------------------------
     | HTTP Access Only
